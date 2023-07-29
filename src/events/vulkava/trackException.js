391fc69f3d6c96c8a5024d9ae920ea0450d1d4f4
AlexGoodlife <novaisboavida@gmail.com>
2023-07-29 21:07:58 +0100

    fixed bug in track exception

--- src/events/vulkava/trackException.js
+++ src/events/vulkava/trackException.js
@@ -6,7 +6,7 @@ module.exports = {
   name : "trackException",
   async execute(client, player, track, error){
     const channel = client.channels.cache.get(player.textChannelId);
-    const embed = new EmbedBuilder().setDescription(`TRACK EXCEPTION ON :  \ ${track.title}\n ${error.message} `);
+    const embed = new EmbedBuilder().setDescription(`TRACK EXCEPTION: ${error.message} `);
     embed.setColor(embedColor);
     await channel.send({embeds : [embed]});
     player.skip(); 
